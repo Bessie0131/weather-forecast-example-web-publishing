c6b4af6d8ca312eb26c2db807025eac3a9662f40
Bessie0131 <byee0003@student.monash.edu>
2020-10-27 08:20:35 +0800

    Updates

---
+++ precache-manifest.1e9f326f6bc6c85338eea4327257b8f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffeb542e4695e119eda85f00b8b36f61",
+    "url": "/weather-forecast-example-web-publishing/index.html"
+  },
+  {
+    "revision": "58aa3ebc10b3d9e075f5",
+    "url": "/weather-forecast-example-web-publishing/static/css/main.1bf83596.chunk.css"
+  },
+  {
+    "revision": "2e19b471e2af7bd1ca6b",
+    "url": "/weather-forecast-example-web-publishing/static/js/2.f5df116d.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/weather-forecast-example-web-publishing/static/js/2.f5df116d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58aa3ebc10b3d9e075f5",
+    "url": "/weather-forecast-example-web-publishing/static/js/main.b7d853bc.chunk.js"
+  },
+  {
+    "revision": "ef161a0880cfb098c035",
+    "url": "/weather-forecast-example-web-publishing/static/js/runtime-main.43017aaa.js"
+  },
+  {
+    "revision": "e78d3d0558952175feb1d2061afc1875",
+    "url": "/weather-forecast-example-web-publishing/static/media/vineyard.e78d3d05.jpg"
+  }
+]);
